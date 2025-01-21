e3eb34b6b5ee055a206a1b274569a6ed5d522386
Shubh Nawani <shubhnawani@outlook.com>
2025-01-21 16:52:25 +0530

    check

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).send('Sorry, we could not find that!');
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
